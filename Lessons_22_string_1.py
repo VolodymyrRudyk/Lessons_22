37c791bd8f5e1eb7cd7b6dfeb556e3ae77274e8c
Volodymyr Rudyk <111973798+VolodymyrRudyk@users.noreply.github.com>
2022-11-14 16:52:09 +0200

    Add files via upload

---
+++ Lessons_22_string_1.py
@@ -0,0 +1,24 @@
+print('shrubbery', "shrubbery")
+print('knight"s', "knight's")
+
+title = "Meaning "  'of ' "Life"      #неявна конкатенація
+print(title)
+
+print('knight\'s', "knight\"s")
+
+s = 'a\0b\0c'
+print(s)
+print(len(s))
+
+s = '\001\002\003'
+print(s)
+print(len(s))
+
+S = "s\tp\na\x00m"
+print(S)
+print(len(S))
+
+mantra = """Alweys look
+on the bright
+side of life."""
+print(mantra)
